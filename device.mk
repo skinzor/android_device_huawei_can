6c95997198d298c687d56f0c4fc8375b7f3b6cb5
skinzor <robertpopescu95@yahoo.com>
2017-07-04 18:05:26 +0300

    device: enable TARGET_BOOTANIMATION_HALF_RES
    *Is supposed to speed up the boot process and make the bootanimation smoother.

--- device.mk
+++ device.mk
@@ -56,6 +56,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
