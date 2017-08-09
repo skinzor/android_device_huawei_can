3e66b65128467b78d061ac7152f601b1cb59a36f
skinzor <robertpopescu95@yahoo.com>
2017-08-09 13:56:12 +0300

    can: add capability.xml

--- device.mk
+++ device.mk
@@ -148,6 +148,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/capability.xml:system/etc/capability.xml \
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
