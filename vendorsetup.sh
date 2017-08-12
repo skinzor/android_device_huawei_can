e5bb44ba93c26c3402eceb8b4b1cdf8f9c770c97
skinzor <robertpopescu95@yahoo.com>
2017-08-12 18:59:08 +0300

    can: omni-fy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo lineage_can-user
-add_lunch_combo lineage_can-userdebug
-add_lunch_combo lineage_can-eng
+add_lunch_combo omni_can-user
+add_lunch_combo omni_can-userdebug
+add_lunch_combo omni_can-eng
 sh device/huawei/can/patches/apply.sh
