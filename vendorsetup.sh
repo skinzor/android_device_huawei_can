75502b90de856ff25a248cde7de159f6f7d4f0fd
skinzor <robertpopescu95@yahoo.com>
2017-09-16 11:17:34 +0300

    can: AOSP bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo lineage_can-user
-add_lunch_combo lineage_can-userdebug
-add_lunch_combo lineage_can-eng
-sh device/huawei/can/patches/apply.sh
+add_lunch_combo aosp_can-user
+add_lunch_combo aosp_can-userdebug
+add_lunch_combo aosp_can-eng
+# sh device/huawei/can/patches/apply.sh
