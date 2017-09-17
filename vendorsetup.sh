03b736c493986a9fc50ce5cf84e5cb2c3bc7c27a
skinzor <robertpopescu95@yahoo.com>
2017-09-17 12:57:24 +0300

    can: lineage -> cm
    Because mer-hybris manifest is still using CM.
    
    https://github.com/mer-hybris/android/tree/hybris-13.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-add_lunch_combo lineage_can-user
-add_lunch_combo lineage_can-userdebug
-add_lunch_combo lineage_can-eng
-sh device/huawei/can/patches/apply.sh
+add_lunch_combo cm_can-user
+add_lunch_combo cm_can-userdebug
+add_lunch_combo cm_can-eng
